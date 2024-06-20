e4f0851c69e4bb7dc010dfba74dc8be4045b0a63
Haoming Wu <104544257+HomingWU@users.noreply.github.com>
2024-06-20 15:57:37 -0400

    components created

---
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-item-detail.component.html',
+  styleUrl: './product-item-detail.component.css'
+})
+export class ProductItemDetailComponent {
+
+}
